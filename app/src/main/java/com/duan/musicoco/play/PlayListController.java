82e75474386ef822f47d19707b3e5b3d3f62dcef
DuanJiaNing <aimeimeits@gmail.com>
2017-07-04 21:07:19 +0800

    add OnThemeChang and PlayListController

--- app/src/main/java/com/duan/musicoco/play/PlayListController.java
+++ app/src/main/java/com/duan/musicoco/play/PlayListController.java
@@ -555,7 +555,6 @@ public class PlayListController implements
                 builder.append(' ').append('(').append(num).append(')');
 
             playMode.setText(builder.toString());
-
         }
     }
 
