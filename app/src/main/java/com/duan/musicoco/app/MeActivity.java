8cb5fd2afde710e2bfabce5f55e2325196300aaa
DuanJiaNing <aimeimeits@gmail.com>
2017-08-24 16:41:26 +0800

    add MeActivity

--- app/src/main/java/com/duan/musicoco/app/MeActivity.java
+++ app/src/main/java/com/duan/musicoco/app/MeActivity.java
@@ -2,12 +2,8 @@ package com.duan.musicoco.app;
 
 import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.duan.musicoco.R;
 import com.duan.musicoco.app.interfaces.ThemeChangeable;
